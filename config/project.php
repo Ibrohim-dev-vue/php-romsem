46045187172f198f98470d2ba5ec1370307243e8
Ibrohim-dev-vue <ibrohimavazov98@gmail.com>
2023-05-11 13:54:59 +0500

    ...
    ...

---
+++ config/project.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'date_format'            => 'Y-m-d',
+    'time_format'            => 'H:i:s',
+    'datetime_format'        => 'Y-m-d H:i:s',
+    'moment_date_format'     => 'YYYY-MM-DD',
+    'moment_time_format'     => 'HH:mm:ss',
+    'moment_datetime_format' => 'YYYY-MM-DD HH:mm:ss',
+    'supported_languages'    => [
+        [
+            'title'      => 'Russian',
+            'short_code' => 'ru',
+        ],
+    ],
+    'registered_user_role' => 'User',
+
+];
